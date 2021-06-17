3354eeba3a1d3a85bca527967681b5da046caccf
Duy Anh <duyanhnguyen1831998@gmail.com.vn>
2021-06-17 20:13:14 +0700

    {AnhND] Resolve conflict

--- src/main/java/com/gcl/crm/service/EmployeeServiceImpl.java
+++ src/main/java/com/gcl/crm/service/EmployeeServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -25,8 +24,4 @@ public class EmployeeServiceImpl implements EmployeeService{
         List<Employee> employees = employeeRepository.findAllByStatusNot(EmployeeStatus.OFF_WORKING);
         return employees;
     }
-
-
-
-
 }
